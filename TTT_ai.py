9b8b3e84b737d90b4e77870916175f790ffaa61d
flanman1866 <flanman1866@gmail.com>
2016-10-24 09:31:28 -0400

    got predict to work for a single row input x. Still need to train weights and update predict to work for vec or mat

--- TTT_ai.py
+++ TTT_ai.py
@@ -33,6 +33,9 @@ ai_move
     return
 '''
 
+def sigmoid(z):
+    sig = 1/(1+np.exp(-z))
+    return sig
 def NN_randInitWeights(Lin,Lout):
     epsilon_init = np.sqrt(6)/np.sqrt(Lin+Lout)
     theta = np.random.rand(Lout,Lin + 1) # generate random theta weights from 0-1
@@ -40,27 +43,69 @@ def NN_randInitWeights(Lin,Lout):
     return theta
 
 def NN_predict(Theta1,Theta2,X):
-    m = len(X[:,0])
+    ## hacking to only predict on a single X vec for now, change to handle matrix or vec later
+    X = np.asarray(X)
+    print('len x:',len(X))
+    X = np.reshape(X,(1,len(X)))
+    print('shape x:',np.shape(X))
+    print('x:',X)
+    
+    m=len(X[:,0])
+    print('m',m)
+##    m = np.size(X[:,0])
     num_classes = len(Theta2[:,0]) # number of nodes in the output layer
-    pred = np.zeros((num_classes,1))
-    return label
+    pred = np.zeros((num_classes,1)) # defaults to dtype np float 64
+
+    # check if the data set had bias units already added, add them if not
+##    if not np.all(X[:,0]==1): 
+##        X = np.concatenate((np.ones((m,1)),X),1)
+    # hack job here
+    
+    if np.size(X[0,:]) == 9:
+        X = np.insert(X,0,1) # inserts 1 into position 0 of X
+        print("added bias unit, X:",X)
+##    print('theta1 shape:',np.shape(Theta1))
+##    print('Xt shape:',np.shape(np.transpose(X)))
+    z2 = np.dot(Theta1,np.transpose(X))
+    print('z2 shape:',np.shape(z2)) # 9x15
+##    a2 = np.concatenate((np.ones((1,m)),sigmoid(z2)),0) # hack
+    a2 = np.insert(sigmoid(z2),0,1)
+    print('a2 shape:',np.shape(a2)) # 10x15 since added bias node j0
+    z3 = np.dot(Theta2,a2)
+    print('z3 shape:',np.shape(z3)) #
+    a3 = class_conf = sigmoid(z3)
+    predict = np.where( class_conf == np.amax(class_conf))
+    pred_conf = np.amax(class_conf)
+    return predict, pred_conf, class_conf
 
 ## load training data (just good for now)
 if os.path.isfile('positive_memories.npy'):
     good_data = np.load('positive_memories.npy')
     print("success loading good data")
     print(good_data)
-    
+
+# splitting inputs and outputs
 m = len(good_data[:,0]) # number of training examples
+X = good_data[:,:-1]
+##X = np.concatenate((np.ones((m,1)),X),1) # add ones for bias units
+y = good_data[:,len(good_data[0,:])-1]
+y = np.reshape(y,(m,1)) # reshape to a column
+
 
 ## corresponds to a NN architecure with 3 layers
 input_layer_size = 9
 hidden_layer_size = 9
 num_labels = 9
 
-Theta1 = NN_randInitWeights(input_layer_size,hidden_layer_size)
+Theta1 = NN_randInitWeights(input_layer_size,hidden_layer_size) 
 ##print("theta1",Theta1)
 Theta2 = NN_randInitWeights(hidden_layer_size, num_labels)
 ##print("theta2",Theta2)
+
+# func has a hack to only work for a vec X input currently
+pred_label, pred_confidence, all_confidences = NN_predict(Theta1,Theta2,X[2,:])
+print('prediction:',pred_label)
+print('prediction confidence:', pred_confidence)
+print('Full class predictions:', all_confidences)
 
 
